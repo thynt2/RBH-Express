91b2f6a4cbfb620107b7254747a6c04b37d7be10
Nguyen Thanh Thy <thynt2@gmail.com>
2022-09-20 19:34:00 +0700

    update report

--- src/test/java/appHooks/ApplicationHooks.java
+++ src/test/java/appHooks/ApplicationHooks.java
@@ -1,6 +1,5 @@
 package appHooks;
 
-import com.google.common.io.Files;
 import common.GlobalConstants;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -17,16 +16,30 @@ import org.openqa.selenium.remote.UnreachableBrowserException;
 
 
 import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 public class ApplicationHooks {
     private static WebDriver driver;
-    private static final Logger log = Logger.getLogger(ApplicationHooks.class.getName());
 
-    @Before
+    @Before(order = 0)
+    public void deleteReportScreenshot() {
+        try {
+            String workingDir = System.getProperty("user.dir");
+            System.out.println(workingDir);
+            String reportFolderPath = workingDir + "/reports";
+            File file = new File(reportFolderPath);
+            File[] listOfFiles = file.listFiles();
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    new File(listOfFiles[i].toString()).delete();
+                }
+            }
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    @Before(order = 1)
     public synchronized static WebDriver openAndQuitBrowser() {
         String browser = System.getProperty("BROWSER");
         if (driver == null) {
@@ -72,13 +85,10 @@ public class ApplicationHooks {
         return driver;
     }
 
+
     public static void close() {
-        try {
-            if (driver != null) {
-                openAndQuitBrowser().quit();
-            }
-        } catch (UnreachableBrowserException e) {
-            throw new RuntimeException("Can not close the browser");
+        if (driver != null) {
+            openAndQuitBrowser().quit();
         }
     }
 
@@ -88,15 +98,12 @@ public class ApplicationHooks {
             close();
         }
     }
-    @After(order = 1)
+
+    @After
     public void tearDown(Scenario scenario) {
-//		if (scenario.isFailed()) {
-        // take screenshot:
         String screenshotName = scenario.getName().replaceAll(" ", "_");
         byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
         scenario.attach(sourcePath, "image/png", screenshotName);
-        System.out.println("tearDown");
-//		}
     }
 
 }
